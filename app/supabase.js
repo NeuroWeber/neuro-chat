7573be7ce66356e115971c710ed2df1c799e45b0
NeuroWeber <neuroweb360@gmail.com>
2026-07-08 04:06:32 +0100

    reconfigured the environment variables

--- app/supabase.js
+++ app/supabase.js
@@ -1,7 +1,7 @@
-import { createClient } from '@supabase/supabase-js'
+import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = 'https://ioyxaxuksriryiqkyoqo.supabase.co'
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImlveXhheHVrc3JpcnlpcWt5b3FvIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODIzMzY4MjksImV4cCI6MjA5NzkxMjgyOX0.Su3e0XAiLNaAgANZLOoqtKq-l5Ec0ek0NtqDBQ5moaY'
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
+const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
 
 // Create a single supabase client for interacting with your database
-export const supabase = createClient(supabaseUrl, supabaseAnonKey)
+export const supabase = createClient(supabaseUrl, supabaseAnonKey);
